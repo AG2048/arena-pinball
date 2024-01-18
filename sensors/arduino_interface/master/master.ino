17f102c7200f567bceac609fe2fee11f164d926d
Kevin Wang <59260907+Togohogo1@users.noreply.github.com>
2024-01-17 23:01:31 -0500

    Interface files -> new folder, blink master/slave

---
+++ sensors/arduino_interface/master/master.ino
@@ -0,0 +1,66 @@
+/*
+Copy older code or rewrite it
+
+send() and receive() for the master
+
+Master handling user input and i2c
+
+Master will be doing polling
+
+
+2 arduinos:
+- master blinks 5 times, and let slave blink 5 times
+- After slave blinks 7 times, and let the master blink back that many time
+
+Error code system
+- e.g. if anything transmits all 1s, then its error
+- Master: send me your location
+- Master: what is your location
+- Slave: ???
+- Slave: respond the error code
+*/
+
+#include <Wire.h>
+
+void setup() {
+  Wire.begin(); // join i2c bus (address optional for master)
+  Serial.begin(9600);
+}
+
+int x = 0;
+
+void loop()
+{
+  // First, blink 5 times
+  // Is this to simulate polling?
+  // Each time that it loops back to the beginning
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(250);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(250);
+  }
+
+  // Instruct the slave to blink 5 times
+  Wire.beginTransmission(4);
+  Wire.write(5);            // Send the number of blinks to the slave
+  Wire.endTransmission();
+
+  // Wait for slave to return data
+  int requested_data = 0;
+  while (requested_data != 0) {
+    // Request 1 byte from slave @ address 4
+    requested_data = Wire.requestFrom(4, 1);
+  }
+  int slaveBlinkCount = Wire.read();
+
+  for (int i = 0; i < slaveBlinkCount; i++) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(250);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(250);
+  }
+
+  // 5 second delay
+  delay(5000);
+}
